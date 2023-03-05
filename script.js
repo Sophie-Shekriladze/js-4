e50716b18fe45c32ade07cbdc277494b83c17607
Sophie Shekriladze <55558347+Sophie-Shekriladze@users.noreply.github.com>
2023-03-06 02:03:09 +0400

    add 4

---
+++ script.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// 1.
+// let array = [5, 25, 89, 120, 36]
+// array.push('javascript', 'python')
+// // console.log(array);
+// array = ['html', 'css', ...array]
+// // console.log(array.length);
+// array.length = (array.length - 1)
+// array.shift()
+// console.log(array);
+
+// 2.
+// let fruits = ['ფორთოხალი', 'ბანანი', 'მსხალი']
+// // console.log(fruits.length);
+// fruits.push('ვაშლი', 'ანანასი')
+// // console.log(fruits);
+// fruits = ['საზამთრო', ...fruits]
+// // console.log(fruits.length);
+
+// fruits[2] = 'მანგო'
+// fruits.push(fruits[2])
+// // console.log(fruits.length);
+
+// fruits.length = (fruits.length - 1)
+// fruits.shift()
+
+// console.log(fruits.length);
+
+// 3.
+
+// let array =[1, 2, 3, 4, 5]
+// array.splice (3, 0, 'a', 'b', 'c')
+// console.log(array);
+
+// 4.
+// let arr = [1, 2, 3, 4, 5];
+// let arrSum = 0;
+// arr.forEach((sum) => {
+//   arrSum += sum;
+// });
+
+// console.log(arrSum);
+
+5.
+function array(...numbers) {
+    for (let i = 0; array.length <= 100; i--) {
+        console.log(array[i]);
+        }  
+} 
+
+array()
